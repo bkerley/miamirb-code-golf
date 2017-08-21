70df47c3a713ae8bbbd3b8ea7d3a3aa7e7f00e53
Bryce Kerley <bkerley@brycekerley.net>
2017-08-21 19:30:38 -0400

    100 club

--- 01-fizzbuzz/bryce.rb
+++ 01-fizzbuzz/bryce.rb
@@ -1,11 +1,1 @@
-(0..100).each do |i|
-  puts (if i % 3 == 0 && i % 5 == 0
-    "fizz buzz"
-  elsif i % 3 == 0
-    "fizz"
-  elsif i % 5 == 0
-    "buzz"
-  else
-    i
-  end
-end
+(0..100).each{|i|puts(((i%3==0&&i%5==0and"fizz buzz")or(i%3==0and"fizz")or(i%5==0and"buzz")or(i)))}
