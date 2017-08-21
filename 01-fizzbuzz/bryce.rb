8171850a0846b0d2efe9961745b4596fb6658528
Bryce Kerley <bkerley@brycekerley.net>
2017-08-21 19:31:41 -0400

    resigned the 100 club

--- 01-fizzbuzz/bryce.rb
+++ 01-fizzbuzz/bryce.rb
@@ -1,1 +1,1 @@
-(0..100).each{|i|puts(((i%3==0&&i%5==0and"fizz buzz")or(i%3==0and"fizz")or(i%5==0and"buzz")or(i)))}
+(0..100).each{|i|puts(((i%3==0&&i%5==0&&"fizz buzz")or(i%3==0&&"fizz")or(i%5==0&&"buzz")or(i)))}
